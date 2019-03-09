c9ae266651d095259b6d50e721e2dd5ab3e87fd5
KolesnikAndrew <kolesnik.andrew.nik@gmail.com>
2019-03-09 12:09:06 +0200

    Add Change

---
+++ calc.py
@@ -0,0 +1,15 @@
+from kivy.app import App
+from kivy.uix.button import Button
+
+class MyApp(App):
+    def build(self):
+        return Button(text = "Button", font_size = 20, on_press = self.btn_press)
+    def btn_press(self, instance):
+        print("Button is pressed")
+        if instance.text == "iamPressed":
+            instance.text = "Pressed"
+        else:
+            instance.text = "iamPressed"
+
+if __name__ == "__main__":
+    MyApp().run()
